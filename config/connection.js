27ea78f2d911adc9a473aa745533fa52acd984b7
chesteraustin <chesteraustin@gmail.com>
2017-07-28 12:34:38 -0700

    update connection for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,17 @@
 (function(){
 	var mysql = require('mysql');
 
-	var con = mysql.createConnection({
-	host: "localhost",
-	user: "username",
-	password: "password",
-	database: "bootcamp_db"
+	if (process.env.JAWSDB_URL) {
+		connection = mysql.createConnection(process.env.JAWSDB_URL);
+	} else {
+		var connection = mysql.createConnection({
+		host: "localhost",
+		user: "username",
+		password: "password",
+		database: "bootcamp_db"
 	});
+	}
 
-	module.exports = con;
+	connection.connect()
+	module.exports = connection;
 })();
